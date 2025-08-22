1af328fe1a48304a569de0c83085d8b85b91b220
Hoàng Xuân Bách <bachbeo2007@gmail.com>
2025-08-22 21:00:19 +0700

    update template

--- template.cpp
+++ template.cpp
@@ -1,14 +1,8 @@
 #include<bits/stdc++.h>
-using namespace std; 
-#define int long long
+using namespace std;
+#define int long long 
 #define pii pair<int,int>
-#define piii pair<pii,int> 
-#define fi first
-#define se second
-const int inf = 1e18;
 const int mod = 998244353;
-const int inv2 = (mod+1)/2;
-const int maxn = 4e5+5;
 int power(int a,int n){
     int res=1;
     while(n){
@@ -17,31 +11,19 @@ int power(int a,int n){
     }
     return res;
 }
-
-int fac[maxn],dfac[maxn],inv[maxn];
-void combi(int n){
-    fac[0]=1;
-    for(int i=1;i<=n;i++) fac[i]=fac[i-1]*i%mod;
-    dfac[n]=power(fac[n],mod-2);
-    for(int i=n;i>=1;i--){
-        dfac[i-1]=dfac[i]*i%mod;
-        inv[i]=dfac[i]*fac[i-1]%mod;
-    }
-}
-int C(int n,int k){
-    if(n<k || n<0 || k<0) return 0;
-    return fac[n]*dfac[k]%mod*dfac[n-k]%mod;
-}
+ 
+const int maxn = 2e5+5;
+const int inf = 1e18;
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rand_int(int l,int r){
     return l+abs((int)rng())%(r-l+1);
 }
 
-void solve(){   
+void solve(){
 
 }
-
+ 
 signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
